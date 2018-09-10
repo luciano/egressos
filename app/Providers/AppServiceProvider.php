3c654aee195fdb79bf0a790e41f2a7cac29ae437
Luciano Silva <luciano.tj2011@gmail.com>
2018-09-10 10:00:08 -0300

    creating admin table and migrating it into the DB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
+     * Needed to use strings in the migration's files
      * @return void
-     */
+     */  
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
