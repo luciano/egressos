29e4aa3da74894ce1b04ba6ae8d5beb19b73a6b7
Luciano Silva <luciano.tj2011@gmail.com>
2018-09-25 13:36:33 -0300

    backup - user fill the questions information

---
+++ resources/views/components/questions.blade.php
@@ -0,0 +1,81 @@
+<p>Preencher perguntas aqui</p>
+
+<p>Pegar dados especificos.. Tipo Tema do TCC, Area de Formação</p>
+
+<p>// user ok</p>
+<p>ID -> nao necessario</p>
+<p>Login -> cpf</p>
+<p>Senha -> criptografar</p>
+
+<p>// student ok</p>
+<p>-Nome </p>
+<p>-Email</p>
+<p>-Sexo</p>
+<p>-DataNasc</p>
+<p></p>
+<p>// address</p>
+<p>-Endereco</p>
+<p>-Telefone</p>
+<p>-Pais</p>
+<p>-Estado</p>
+<p>-Cidade</p>
+<p>-Cep</p>
+<p></p>
+<p>// user recover password</p>
+<p>// pergunta e resposta secreta pra recuperar senha...</p>
+<p>Pergunta</p>
+<p>Resposta</p>
+<p></p>
+<p>// pode ser mais de uma</p>
+<p>Graduacao</p>
+<p>Anograd</p>
+<p>Posgraduacao</p>
+<p>Anoposgra</p>
+
+<p>// pode ser mais de uma empresa, pra ter historico</p>
+<p>-Situacao -> empregado ou desempregado</p>
+<p>-Empresa -> nome da empresa que trabalha atual</p>
+<p>-Departamento -> nome departamento</p>
+<p>-Enderecoemp -> endereco empresa</p>
+<p>-Telefoneemp -> telefone empresa</p>
+<p>-Site -> site empresa</p>
+<p>-Natureza -> tipo empresa (Publica, Privada, Capital Misto, Sindicato, Cooperativa, Fundação, Outros)</p>
+<p>-Outranatureza -> se marca outros.. ou salvar no mesmo campo</p>
+
+<h3>Dados Pessoais</h3>
+ 	
+<p>Sexo:</p>
+<p>Data de nascimento:</p>
+<p>Endereço:	 </p>
+<p>Telefone:	 </p>
+<p>País:	 </p>
+<p>Estado:	 </p>
+<p>Cidade:	 </p>
+<p>CEP:	 </p>
+
+<h3>Dados Academicos</h3>
+<p>Graduação</p>	
+<p>Ano de Conclusão	 </p>
+
+<p>Pós-Graduação	 </p>
+<p>Ano de Conclusão	</p>
+
+<h3>Dados Profissionais</h3>
+<p>Situação atual:	 () Desempregado  () Empregado</p>
+<p>Empresa:	 </p>
+<p>Departamento:</p>	 
+<p>Endereço:	 </p>
+<p>Telefone:	 </p>
+<p>Site:	 </p>
+<p>Natureza da empresa:	 </p>
+<p>Qual:	</p>
+
+<p>// perguntas</p>
+<p>P1 -> Você já trabalhava neste estabelecimento antes de concluir o curso que fez na DEAD/UFVJM?</p>
+<p>P2 -> Se você NÃO trabalhava neste estabelecimento, o curso que você concluiu na DEAD/UFVJM, contribuiu para que você conseguisse seu emprego atual?</p>
+<p>P3a -> Se você já trabalhava nesta empresa, responda: a) Qual a função que exercia antes da conclusão de seu curso?</p>
+<p>P3b -> Você mudou de cargo ou setor depois de ter concluído o curso?</p>
+<p>P3c -> Esta mudança de função tratou-se de promoção em virtude da conclusão de seu curso?</p>
+<p>P4 -> Hoje, o curso que fez na DEAD/UFVJM tem importância no seu sucesso profissional?</p>
+<p>P5a -> Você tem interesse em cursar algum curso de pós-graduação Lato Sensu correlato com sua área de formação?</p>
+<p>P5b -> Caso afirmativo, favor especificar qual</p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">USER Dashboard</div>
+@if (auth()->user()->created_at == auth()->user()->updated_at)
+    @section('content')    
+        <div class="container">
+            @component('components.questions')
+            @endcomponent
+        </div>
+    @endsection
+@else
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">USER Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @component('components.who')    
-                    @endcomponent
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        @component('components.who')    
+                        @endcomponent
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
+@endif
