e9e233b3420028f3916deae4a285e9ab157253c6
Luciano Silva <luciano.tj2011@gmail.com>
2019-02-11 10:58:09 -0200

    adjusting css index admin page

--- resources/views/events/index-admin.blade.php
+++ resources/views/events/index-admin.blade.php
@@ -1,22 +1,18 @@
 @extends('admin.layouts.nav')
 
 @section('content')
-    <h1>Eventos para Admin</h1>
-    <h5>{{Carbon\Carbon::now()->format('d/m/Y H:i:s')}}</h5>
+    <h3 class="text-center">Gerenciar Eventos</h3>
 
-    <p></p><a href="{{route('admin.events.create')}}" class="button primary">Criar Evento</a> </p>
+    <div align="center" class="padding-2">
+        <a href="{{route('admin.events.create')}}" class="button large rounded bordered shadow primary ">Criar Evento</a>
+    </div>
     
     @if (count($events) > 0)
         @foreach ($events as $event)
-            <div class="well">
-                <div class="row">
-                    {{-- <div class="col-md-4 col-sm-4">
-                        <img style="width: 100%" src="/storage/cover_images/{{$event->cover_image}}" />
-                    </div> --}}
-                    <div class="col-md-8 col-sm-8">
-                        <h3><a href="{{route('admin.events.show', $event->id)}}">{{$event->title}}</a></h3>
+            <div align="center">
+                <div align="left" class="callout width-75">
+                <h3><a href="{{route('admin.events.show', $event->id)}}">{{$event->title}}</a></h3>
                         <small>Escrito em {{$event->created_at->format('d/m/Y')}} às {{$event->created_at->format('H:i')}} por {{$event->admin->name}}</small>
-                    </div>
                 </div>
             </div>
         @endforeach
--- resources/views/news/index-admin.blade.php
+++ resources/views/news/index-admin.blade.php
@@ -1,24 +1,26 @@
 @extends('admin.layouts.nav')
 
 @section('content')
-    <h1>Notícias para Admin</h1>
-    <h5>{{Carbon\Carbon::now()->format('d/m/Y H:i:s')}}</h5>
+    <h3 class="text-center">Gerenciar Notícias</h3>
 
-    <p></p><a href="{{route('admin.news.create')}}" class="button primary">Criar Notícia</a> </p>
+    <div align="center" class="padding-2">
+        <a href="{{route('admin.news.create')}}" class="button large rounded bordered shadow primary ">Criar Notícia</a>
+    </div>
     
     @if (count($news) > 0)
         @foreach ($news as $new)
-            <div class="well">
-                <div class="row">
-                    {{-- <div class="col-md-4 col-sm-4">
-                        <img style="width: 100%" src="/storage/cover_images/{{$new->cover_image}}" />
-                    </div> --}}
-                    <div class="col-md-8 col-sm-8">
-                        <h3><a href="{{route('admin.news.show', $new->id)}}">{{$new->title}}</a></h3>
-                        <small>Escrito em {{$new->created_at->format('d/m/Y')}} às {{$new->created_at->format('H:i')}} por {{$new->admin->name}}</small>
-                    </div>
+            <div align="center">
+                <div align="left" class="callout width-75">
+                    <h3><a href="{{route('admin.news.show', $new->id)}}">{{$new->title}}</a></h3>
+                    <small>Escrito em {{$new->created_at->format('d/m/Y')}} às {{$new->created_at->format('H:i')}} por {{$new->admin->name}}</small>
                 </div>
             </div>
+            
+            {{-- 
+            <div class="col-md-4 col-sm-4">
+                <img style="width: 100%" src="/storage/cover_images/{{$new->cover_image}}" />
+            </div> 
+            --}}
         @endforeach
         {{-- using pagination automatically --}}
         {{$news->links()}} 
